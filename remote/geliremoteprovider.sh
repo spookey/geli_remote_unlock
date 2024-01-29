7d4e0947025400441ab9e567907a2b7843ad7b2d
Frieder Griesshammer <frieder.griesshammer@der-beweis.de>
2024-01-29 21:23:01 +0100

    Convert indentation

--- remote/geliremoteprovider.sh
+++ remote/geliremoteprovider.sh
@@ -21,33 +21,33 @@ IFS=' ' read -r COMMAND FILE <<< "$INPUT"
 
 # helper function to signal some error and exit
 err() {
-	(>&2 echo "error")
-	[ -n "$*" ] && (>&2 echo "$*")
-	exit 1
+    (>&2 echo "error")
+    [ -n "$*" ] && (>&2 echo "$*")
+    exit 1
 }
 # helper function to safely retrieve file contents
 get() {
-	[ ! -d "$1" ] && err "directory not found"
-	[ ! -n "$2" ] && err "empty request"
-	case $2 in
-		*\.\.*) err "no double dots allowed" ;;
-		*/*) err "no slashes allowed" ;;
-	esac
-	[ ! -r "$1/$2" ] && err "file not found"
-	cat "$1/$2"
+    [ ! -d "$1" ] && err "directory not found"
+    [ ! -n "$2" ] && err "empty request"
+    case $2 in
+        *\.\.*) err "no double dots allowed" ;;
+        */*) err "no slashes allowed" ;;
+    esac
+    [ ! -r "$1/$2" ] && err "file not found"
+    cat "$1/$2"
 }
 
 # parse known commands
 case $COMMAND in
-	keyfile)
-		get "$DIR_KEYS" "$FILE"
-		;;
-	passphrase)
-		get "$DIR_PASS" "$FILE"
-		;;
-	*)
-		err
-		;;
+    keyfile)
+        get "$DIR_KEYS" "$FILE"
+        ;;
+    passphrase)
+        get "$DIR_PASS" "$FILE"
+        ;;
+    *)
+        err
+        ;;
 esac
 
 exit 0
