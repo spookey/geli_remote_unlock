b666484c85fe1e5a07aeb09b853ae7a1f12b2a58
Frieder Griesshammer <frieder.griesshammer@der-beweis.de>
2024-01-30 20:23:15 +0100

    Create alert mail using printf statements

--- remote/geliremotealert.sh
+++ remote/geliremotealert.sh
@@ -11,27 +11,27 @@ source "$SELF_DIR/config.sh"
 TIME="$(date -Iseconds)"
 
 {
-    echo "From: $MAIL_FROM"
-    echo "To: $MAIL_RCPT"
-    echo "Subject: $MAIL_SUBJ ($TIME)"
-    echo "Content-Type: text/plain"
-    echo
-    echo
-    echo "Client"
-    echo -e "\\t" "$SSH_CLIENT"
-    echo
-    echo "Connection"
-    echo -e "\\t" "$SSH_CONNECTION"
-    echo
-    echo "Command"
-    echo -e "\\t" "$SSH_ORIGINAL_COMMAND"
-    echo
-    echo "TTY"
-    echo -e "\\t" "$SSH_TTY"
-    echo
-    echo "Time"
-    echo -e "\\t" "$TIME"
-    echo
+    printf "From: %s\n" "$MAIL_FROM"
+    printf "To: %s\n" "$MAIL_RCPT"
+    printf "Subject: %s (%s)\n" "$MAIL_SUBJ" "$TIME"
+    printf "Content-Type: %s\n" "text/plain"
+    printf "\n"
+    printf "\n"
+    printf "Client\n"
+    printf "\t%s\n" "$SSH_CLIENT"
+    printf "\n"
+    printf "Connection\n"
+    printf "\t%s\n" "$SSH_CONNECTION"
+    printf "\n"
+    printf "Command\n"
+    printf "\t%s\n" "$SSH_ORIGINAL_COMMAND"
+    printf "\n"
+    printf "TTY\n"
+    printf "\t%s\n" "$SSH_TTY"
+    printf "\n"
+    printf "Time\n"
+    printf "\t%s\n" "$TIME"
+    printf "\n"
 } | sendmail -t
 
 exit 0
